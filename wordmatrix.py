9e95687bb08f3245bc6ef3ac3bae0ca3eef2ade5
aalok-sathe <asathe@mit.edu>
2022-03-22 11:49:53 -0400

    resort to using NetworkX for simplicity

--- wordmatrix.py
+++ wordmatrix.py
@@ -1,65 +1,62 @@
 
 from collections import defaultdict
 
-import graph_tool.all as gt
+import numpy as np
+# import graph_tool.all as gt
 import networkx as nx
+from pyvis.network import Network
+
 
 class WordGraph():
         
     def __init__(self, wordpairs, backend='nx'):
-        
-        if backend == 'gt':
-            g = gt.Graph()
-
-            g.ep.occ = g.new_ep("int")
-            g.vp.upos = g.new_vp("string")
-            g.vp.text = g.new_vp("string")
-
-            self.vertices = defaultdict(lambda: g.add_vertex()) # word, upos -> vertex
-            self.edges = dict() # word, upos -> vertex
-            for w, p in wordpairs:
-
-                w_ = self.vertices[w.text, w.upos]
-                p_ = self.vertices[p.text, p.upos]
-
-                edge_name = w.text, w.upos, p.text, p.upos
-                if edge_name not in self.edges:
-                    self.edges[edge_name] = g.add_edge(w_, p_)
-                e = self.edges[edge_name]
-
-                g.vp.text[w_], g.vp.upos[w_] = w.text, w.upos
-                g.vp.text[p_], g.vp.upos[p_] = p.text, p.upos
-
-                g.ep.occ[e] += 1 
-                
-        elif backend == 'nx':
             
-            g = nx.DiGraph()
-            nodes = {} # tuple -> int
-            edges = defaultdict(int)
-            
-            for w, p in wordpairs:
-                w_ = (w.text,w.upos)
-                p_ = (p.text,p.upos)
-                
-                nodes[w_] = nodes.get(w_, len(nodes))
-                nodes[p_] = nodes.get(p_, len(nodes))
-                
-                g.add_node(nodes[w_], label=w.text, group=w.upos)
-                g.add_node(nodes[p_], label=p.text, group=p.upos)
-                
-                edges[nodes[w_], nodes[p_]] += 1
-                
-            for edge, numocc in edges.items():
-                g.add_edge(*edge, label=numocc)
+        g = nx.DiGraph()
+        nodes = {} # tuple -> int
+        edges = defaultdict(int)
+
+        for w, p in wordpairs:
+            nodes[w] = nodes.get(w, len(nodes))
+            nodes[p] = nodes.get(p, len(nodes))
+
+            g.add_node(nodes[w], label=str(w), group=str(w.upos))
+            g.add_node(nodes[p], label=str(p), group=str(p.upos))
+
+            edges[nodes[w], nodes[p]] += 1
+
+        for edge, numocc in edges.items():
+            g.add_edge(*edge, label=numocc, value=numocc)
                 
         self.g = g
         
+        
+    def subgraph(self, n_nodes) -> nx.DiGraph:
+        if n_nodes in {None, -1, 0}:
+            return self.g
+        return self.g.subgraph(np.random.choice(self.g.nodes, n_nodes))
+        
+        
+    def to_pyvis(self, n_nodes=1_000, notebook=False) -> Network:
+        
+        # net = Network('700px', '1000px', notebook=True)
+        net = Network(height='500px', width='100%', notebook=notebook)
+        net.from_nx(self.subgraph(n_nodes))
+        
+        net.force_atlas_2based()
+        # net.barnes_hut(central_gravity=1, spring_length=100)
+        net.show_buttons(filter_=['physics']) 
+        # net.show(f'nx_{child}-{parent}.html')
+        return net
+    
+    
     def __repr__(self):
         return repr(self.g)
     
     def __str__(self):
         return str(self.g)
+    
+    def __len__(self):
+        return len(self.g)
     
     
     
