e9b8feb27bd076fecbb489b23835d2e9dd20077c
aalok-sathe <asathe@mit.edu>
2022-04-02 23:21:07 -0400

    moved this file elsewhere

--- test/test_cache.py
+++
@@ -1,6 +0,0 @@
-
-from corpus import Corpus
-
-c = Corpus('COCA', cache_dir='./cache', tag='coca_inf', n_sentences=float('inf'))
-# _ = list(c.read()) # initial run-through for constructing a cache
-_ = list(c.extract_edges()) # for computing pairwise occurrence stats
