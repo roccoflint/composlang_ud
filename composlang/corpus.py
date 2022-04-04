022f5dc55810f55a4ab6f713c839809f51772478
aalok-sathe <asathe@mit.edu>
2022-04-03 21:13:10 -0400

    implement ChainedCorpus which is a concatenation of multiple Corpus object caches; misc changes in Corpus implementation

--- composlang/corpus.py
+++ composlang/corpus.py
@@ -1,10 +1,9 @@
-
-import argparse
 import fileinput
 import pydoc
 import time
 import typing
 from collections import Counter, defaultdict
+from functools import reduce
 from pathlib import Path
 
 import numpy as np
@@ -13,11 +12,10 @@ import stanza
 from joblib import Parallel, delayed
 from more_itertools import peekable
 from sqlitedict import SqliteDict
-from stanza.models.common.doc import Document
 from tqdm import tqdm
 
 from composlang.graph import WordGraph
-from composlang.utils import iterable_from_directory_or_filelist, log
+from composlang.utils import iterable_from_directory_or_filelist, log, pathify
 from composlang.word import Word
 
 
@@ -30,7 +28,7 @@ class Corpus:
     The only restriction is that a `sentence_id` column is required in order
     to detect sentence boundaries.
     '''
-    
+
     # determining where to obtain input and how to process it
     _fmt, _sep, _lower = None, None, None
     _files = None
@@ -45,20 +43,20 @@ class Corpus:
 
     # tracking the linguistic features of interest within the corpus
     _token_stats = None # (token:str, upos:str) -> num_occ:int
-    _pair_stats = None # ((token1:str, upos1:str), (token2, upos2)) -> num_occ:int 
-                       # in a child-parent relation in a dependency parse
-    _cache = None
+    _pair_stats = None # ((token1:str, upos1:str), (token2, upos2)) -> num_occ:int
+    # in a child-parent relation in a dependency parse
+    cache = None
 
 
-    def __init__(self, 
-                 directory_or_filelist: typing.Union[Path, str, 
-                                                     typing.Iterable[typing.Union[Path, str]]], 
+    def __init__(self,
+                 directory_or_filelist: typing.Union[Path, str,
+                                                     typing.Iterable[typing.Union[Path, str]]],
 
                  cache_dir: typing.Union[str, Path] = './cache/composlang',
                  cache_tag: str = None,
 
-                 n_sentences: int = None, store: bool = False,
-                 fmt: typing.List[str] = ('sentence_id:int', 'text:str', 'lemma:str', 
+                 n_sentences: int = None, 
+                 fmt: typing.List[str] = ('sentence_id:int', 'text:str', 'lemma:str',
                                           'id:int', 'head:int', 'upos:str', 'deprel:str'),
                  sep: str = '\t', lowercase: bool = True
                  ):
@@ -68,22 +66,30 @@ class Corpus:
         self._lower = lowercase
         self._files = sorted(iterable_from_directory_or_filelist(directory_or_filelist))
         if self._files is None:
-            raise ValueError(f'could not find files at {directory_or_filelist}')
-        
+            log(f'could not find files at {directory_or_filelist}')
+            # raise ValueError(f'could not find files at {directory_or_filelist}')
+
         self._pair_stats = Counter() # (token, token) -> num_occurrences
         self._token_stats = Counter() # token -> num_occurrences
-        
+
         # loads the pre-existing _pair_stats and _token_stats objects, or creates empty ones
         self._cache_dir = cache_dir
         self._cache_tag = cache_tag
         self.cache = self._get_cache()
         self.load_cache()
         self._n_sentences = n_sentences or float('inf') # upper limit for no. of sentences to process
-        
- 
+
+    @classmethod
+    def from_cache(cls, cache_file: typing.Union[str, Path], 
+                   n_sentences=None, fmt=None, sep=None, lowercase=None):
+        path = pathify(cache_file)
+        cache_dir = path.parent
+        cache_tag = path.parts[-1]
+        return cls('/dev/null', cache_dir, cache_tag, n_sentences, fmt, sep, lowercase)
+
     def __len__(self) -> int:
         return self._sentences_seen
-    
+
     # use context manager to handle closing of cache
     def __enter__(self):
         return self # nothing to do here
@@ -115,9 +121,9 @@ class Corpus:
         root /= tag
         return SqliteDict(str(root), flag='c')
 
-    _attrs_to_cache = (('_sentences_seen', int), ('_lines_read', int), 
-                      ('_current_file', lambda: None), ('_pair_stats', Counter), 
-                      ('_token_stats', Counter), ('_files', lambda: None), 
+    _attrs_to_cache = (('_sentences_seen', int), ('_lines_read', int),
+                      ('_current_file', lambda: None), ('_pair_stats', Counter),
+                      ('_token_stats', Counter), ('_files', lambda: None),
                       ('_total', lambda: None), ('_n_sentences', lambda: None))
 
     def load_cache(self, allow_empty=True):
@@ -125,6 +131,7 @@ class Corpus:
         recover core data of this instance from cache 
         '''
         log(f'attempt loading from cache at {self._cache_dir}/{self._cache_tag}')
+        start = time.process_time()
         for attr, default in self._attrs_to_cache:
             try:
                 obj = self.cache[attr]
@@ -133,29 +140,31 @@ class Corpus:
                 log(f'could not find attribute {attr} in cache')
                 if not allow_empty:
                     raise e
-        log(f'successfully loaded cached data from {self._cache_dir}/{self._cache_tag}')
+        end = time.process_time()
+        log(f'successfully loaded cached data from {self._cache_dir}/{self._cache_tag} in {end-start:.3f} seconds')
 
     def to_cache(self):
         '''
         dump critical state data of this instance to cache
         '''
         log(f'caching to {self._cache_dir}/{self._cache_tag}')
+        start = time.process_time()
         for attr, _ in self._attrs_to_cache:
             obj = getattr(self, attr)
             self.cache[attr] = obj
 
-        start = time.process_time()
         self.cache.commit()
         end = time.process_time()
         log(f'successfully cached to {self._cache_dir}/{self._cache_tag} in {end-start:.3f} seconds')
-        
 
-    def read(self, batch_size:int=1_024) -> Document:
+
+    def read(self, batch_size:int=1_024):
         '''
         Reads a parsed corpus file 
         the corpus file is formatted similar to depparse output below, 
         or according to a custom format which specified at instantiation.
         '''
+        from stanza.models.common.doc import Document
         
         dummy_line = lambda: f'{"__EOF__"}{self._sep}' + self._sep.join(map(str, range(6 + 10)))
         # if we are resuming from previous state, we want to skip lines that are already processed.
@@ -191,7 +200,7 @@ class Corpus:
                 if lines_to_skip > 0:
                     lines_to_skip -= 1
                     continue
-                    
+
                 # if line.strip() == '': continue
                 if self._current_file != fileinput.filename():
                     self._current_file = fileinput.filename()
@@ -214,16 +223,16 @@ class Corpus:
                     sentence_batch += [sent]
 
                     if (sents_read := len(sentence_batch)) >= batch_size or self._sentences_seen+sents_read >= n_sentences:
-                        
+
                         self.digest_sentencebatch(sentence_batch)
                         lines_read = sum(len(s.words) for s in sentence_batch)
                         sentence_batch = []
 
                         self._lines_read += lines_read
                         self._sentences_seen += sents_read
-                        if self._total < float('inf'): 
+                        if self._total < float('inf'):
                             T.update(sents_read)
-                        else: 
+                        else:
                             T.update(lines_read)
                         self.to_cache()
 
@@ -236,7 +245,7 @@ class Corpus:
                         anchor_time = this_time
 
                     this_sentence = []
-                    
+
 
                 if self._sentences_seen >= n_sentences:
                     self.to_cache()
@@ -285,12 +294,12 @@ class Corpus:
                 log('ERR:', line, line.strip().split(sep))
                 raise
             doc[label] = value
-        
+
         return doc
 
-    
+
     @classmethod
-    def _extract_edges_from_sentence(cls, 
+    def _extract_edges_from_sentence(cls,
                                      sentence: stanza.models.common.doc.Sentence) -> \
                                      typing.Iterable[typing.Tuple[Word, Word]]:
         edges = []
@@ -301,11 +310,11 @@ class Corpus:
             p = sentence.words[w.head-1]
             edges += [(Word(w.text, w.upos), Word(p.text, p.upos))]
 
-        return edges 
-        
-    
+        return edges
+
+
     def extract_upos_pairs(self,
-                           child_upos, 
+                           child_upos,
                            parent_upos,
                            include_context=False):
         '''
@@ -320,7 +329,7 @@ class Corpus:
                        ) -> 'nx.Graph':
         '''
         '''
-        wg = WordGraph(((w,p) for w,p in self.extract_edges() 
+        wg = WordGraph(((w,p) for w,p in self.extract_edges()
                         if (child_upos is None or w.upos == child_upos) and \
                            (parent_upos is None or p.upos == parent_upos)))
 
@@ -334,10 +343,10 @@ class Corpus:
         optionally, if unique=True, consider each token as one occurrence
         '''
         if unique:
-            return {upos: len(self._upos_token_stats[upos].values()) 
+            return {upos: len(self._upos_token_stats[upos].values())
                     for upos in self._upos_token_stats}
         return Counter(upos for token, upos in self._token_stats)
-    
+
 
     def token_counts(self, upos: typing.Iterable = ()):
         '''
@@ -345,48 +354,76 @@ class Corpus:
         restricted to the given uposes (optional), or all uposes available
         '''
         uposes = set(self._upos_token_stats.keys())
-        if upos: 
+        if upos:
             uposes.intersection_update(set(upos))
-        
+
         token_counts = defaultdict(int)
         for upos in uposes:
             for token in self._upos_token_stats[upos].keys():
-                token_counts[token] += self._upos_token_stats[upos][token] 
-        
+                token_counts[token] += self._upos_token_stats[upos][token]
+
         return token_counts
-                           
-    
+
+
     def extract_combinations(self,
                              child_upos, parent_upos,
                              unique: bool = True,
-                             ) -> typing.Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:    
+                             ) -> typing.Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
 
         child_to_parent = defaultdict(int)
         parent_to_child = defaultdict(int)
-        
+
         for (w,p), ct in self._pair_stats.items():
             if (child_upos == '*' or w.upos == child_upos) and (parent_upos == '*' or p.upos == parent_upos):
                 child_to_parent[w] += 1 if unique else ct
                 parent_to_child[p] += 1 if unique else ct
-                                                   
+
         child_to_parent_arr = np.array(sorted(child_to_parent.values(), key=lambda c:-c))
         parent_to_child_arr = np.array(sorted(parent_to_child.values(), key=lambda c:-c))
-        
+
         child_to_parent_labels = np.array(sorted(child_to_parent.keys(), key=lambda k:-child_to_parent[k]))
         parent_to_child_labels = np.array(sorted(parent_to_child.keys(), key=lambda k:-parent_to_child[k]))
-        
+
         return child_to_parent_arr, child_to_parent_labels, parent_to_child_arr, parent_to_child_labels
-    
-    
-if __name__ == '__main__':
-    p = argparse.ArgumentParser('composlang')
-    p.add_argument('--path', help='Path to directory storing corpus files, or path to a text file',
-                   required=True)
 
-    args = p.parse_args()
-    print(args)
 
-    with Corpus(args.path, 
-                cache_dir='./cache', cache_tag='coca_inf', 
-                n_sentences=float('inf')) as coca:
-        coca.read()
+
+class ChainedCorpus(Corpus):
+
+    def __init__(self,
+                 directory_or_filelist: typing.Union[
+                     Path, str, typing.Iterable[typing.Union[Path, str]]],
+                 cache_dir=None, cache_tag=None,
+                 fmt: typing.List[str] = ('sentence_id:int', 'text:str',
+                                          'lemma:str', 'id:int', 'head:int',
+                                          'upos:str', 'deprel:str'),
+                 sep: str = '\t', lowercase: bool = True):
+
+        # store Corpus objects initialized from cache files in a list
+        corpus_objs = []
+        for path in iterable_from_directory_or_filelist(directory_or_filelist):
+            c = Corpus.from_cache(path)
+            corpus_objs += [c]
+
+        # we want to aggregate statistics of subparts using their cached objects
+        for attr, typ in self._attrs_to_cache:
+            if typ in (int, Counter):
+                # choose the appropriate add function according to class
+                redfn = getattr(typ, '__add__')
+                # construct the reduced (aggregated) object, an int or a Counter, to assign to self
+                ob = reduce(redfn, [getattr(c, attr) for c in corpus_objs])
+                setattr(self, attr, ob)
+
+        # close connection to SQLite after done loading
+        for c in corpus_objs:
+            c._close_cache()
+
+
+    def _get_cache(self, cache_dir, cache_tag):
+        return super()._get_cache(cache_dir, cache_tag)
+    def load_cache(self, allow_empty=True):
+        raise NotImplementedError
+    def to_cache(self, cache_dir, cache_tag):
+        if self.cache is None:
+            self.cache = self._get_cache(cache_dir, cache_tag)
+        super().to_cache()
