de93f5c8131c7d33842ecb15a78afade0b4b15a6
aalok-sathe <asathe@mit.edu>
2022-04-05 15:25:24 -0400

    add argument for n_sentences

--- composlang/__main__.py
+++ composlang/__main__.py
@@ -10,15 +10,17 @@ if __name__ == '__main__':
                                  'if the input is a directory containing multiple files, then the hash of '
                                  'the concatenation of these filenames is used', 
                    default=None, type=str, required=False)
-    p.add_argument('--cache_dir', help='directory to use for caching. by default, $PWD/cache is used.', 
+    p.add_argument('--cache_dir', help='directory to use for caching. by default, $(pwd)/cache is used.', 
                    default='./cache', type=str, required=False)
     p.add_argument('--batch_size', help='batch process sentences with parallelized stats computation. '
                                         'default: 5000',
                    type=int, default=5_000, required=False)
+    p.add_argument('--n_sentences', help='constrain total sentences to process to this number. default: inf',
+                   type=int, default=float('inf'), required=False)
 
     args = p.parse_args()
     print(args)
 
     with Corpus(args.path, cache_dir=args.cache_dir, cache_tag=args.tag, 
-                n_sentences=float('inf')) as coca:
+                n_sentences=args.n_sentences) as coca:
         coca.read(batch_size=args.batch_size)
