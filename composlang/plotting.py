d5cb8ae185a66f0d59d6361eaaaddfd21e755402
aalok-sathe <asathe@mit.edu>
2022-04-05 15:24:35 -0400

    refactor plotting method to accept a dataframe and sort-by column; additional bugfixes

--- composlang/plotting.py
+++ composlang/plotting.py
@@ -1,6 +1,7 @@
     
 import numpy as np
 from matplotlib import pyplot as plt
+from composlang.utils import log
 
 def fit_zipf(freq):
     
@@ -14,39 +15,45 @@ def fit_zipf(freq):
     
     
     
-def plot_rank_freq_distribution(ranked_freq, labels, upos):
+def plot_rank_distribution(df, sort_col='freq', value_col='entropy', label_col='token', 
+                           upos='UPOS?', fit_reference=False):
 
+    view = df.sort_values(sort_col, ascending=False, ignore_index=True)[[value_col, label_col]]
+    ranked_freq = view.iloc[:, 0] + (1 if fit_reference and value_col in ['combinations', 'freq'] else 0)
+    labels = view.iloc[:, 1].to_numpy()
     fig, (left, right) = plt.subplots(1,2, figsize=(20,5))
 
+
     def plot_comparisons(candidate, axes):
-        
         fitted, res = fit_zipf(candidate)
         axes.plot(fitted, '-', label=f'fitted using linregress\n$r={res.rvalue:.2f},p={res.pvalue:.2f}$')
         # axes.fill_between(np.arange(1, len(fitted)+1),
         #                   np.exp(np.log(fitted)-res.stderr), np.exp(np.log(fitted)+res.stderr),
         #                   color='gray', alpha=0.2)
-        np.random.seed(1)
-        #### ZIPFs for comparison
-        for a in np.arange(1.5, 3, .3):
-            z = np.sort(np.random.zipf(a, (len(candidate,))))[::-1]
-            # z = z[z <= max(candidate)+10]
-            axes.plot(z, '--', label=f'draw from Zipf (a={a:.2f})')
+        # np.random.seed(1)
+        # #### ZIPFs for comparison
+        # for a in np.arange(1.5, 3, .3):
+        #     z = np.sort(np.random.zipf(a, (len(candidate,))))[::-1]
+        #     # z = z[z <= max(candidate)+10]
+        #     axes.plot(z, '--', label=f'draw from Zipf (a={a:.2f})')
 
 
     ####################
     #### LEFT
     ####################
-    left.plot(ranked_freq, 'b.' if upos in ('NOUN', 'VERB') else 'r.', label=f'{upos} compos.', 
+    left.plot(ranked_freq, 'b.' if upos in ('NOUN', 'VERB') else 'r.', label=f'{upos}', 
                  linewidth=5, alpha=.7, )
 
-    plot_comparisons(ranked_freq, left)
+    if fit_reference:
+        plot_comparisons(ranked_freq, left)
 
-    left.set(xlabel=f'{upos} compositionality rank',
-                ylabel=f'# lexical that combine with {upos}',
-                yscale='log')
-    left.set_ylim([.9, max(ranked_freq)+10])
+    left.set(xlabel=f'{upos} {label_col}; {sort_col}-rank',
+                ylabel=f'{value_col} (log scale)',
+                yscale='log'
+                )
+    left.set_ylim([min(ranked_freq), max(ranked_freq)+10])
     
-    ulim = np.ceil(np.log(ranked_freq[0])/np.log(2))+1
+    ulim = np.ceil(np.log2(max(ranked_freq)))+1
     ulim = int(ulim)
     left.set_yticks(2**np.arange(ulim), 2**np.arange(ulim))
 
@@ -64,18 +71,21 @@ def plot_rank_freq_distribution(ranked_freq, labels, upos):
     right.plot(ranked_freq, 'b.' if upos in ('NOUN', 'VERB') else 'r.',  label=f'{upos} compos.', 
                  linewidth=5, alpha=.7,  )
 
-    plot_comparisons(ranked_freq, right)
+    if fit_reference:
+        plot_comparisons(ranked_freq, right)
 
-    right.set(xlabel=f'{upos} compositionality rank\n(log scale)',
-                ylabel=f'# lexical items that combine with {upos}\n(log scale)',
+    right.set(xlabel=f'{upos} {label_col}; {sort_col}-rank\n(log scale)',
+                ylabel=f'{value_col} (log scale)',
+                # ylabel=f'# lexical items that combine with {upos}\n(log scale)',
                 xscale='log', yscale='log')
-    right.set_ylim([.9, max(ranked_freq)+10])
+    right.set_ylim([min(ranked_freq), max(ranked_freq)+10])
 
-    ulim = np.ceil(np.log(ranked_freq[0])/np.log(2))+1
+    ulim = np.ceil(np.log2(max(ranked_freq)))+1
     ulim = int(ulim)
     right.set_yticks(2**np.arange(ulim), 2**np.arange(ulim))
     
-    xticks = [*2**np.arange(ulim)] + [*np.arange(2**ulim, len(ranked_freq), (len(ranked_freq)-2**ulim)//3)]
+    ulim = int(np.log2(len(ranked_freq))+1)
+    xticks = [*2**np.arange(ulim)] #+ [*np.arange(2**ulim, len(ranked_freq), (len(ranked_freq)-2**ulim)//3)]
     # log(xticks)
     right.set_xticks(xticks,
                      labels=[f'{a}\n{b:.0e}' for a,b in zip(labels[xticks], xticks)],
