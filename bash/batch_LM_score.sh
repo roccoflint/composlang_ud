351ebc06660b52c91e72b86a900f1a91dc8dc5b9
aalok-sathe <asathe@mit.edu>
2022-05-09 10:13:46 -0400

    some openmind gpus have outdated cuda

--- bash/batch_LM_score.sh
+++ bash/batch_LM_score.sh
@@ -2,10 +2,9 @@
 #SBATCH -c 6
 #SBATCH -t 4:00:00
 #SBATCH --mem=50G
-#SBATCH --gres=gpu
 #SBATCH -p evlab
 #SBATCH -n 1
-#SBATCH --array=1-500%100
+#SBATCH --array=1-501%100
 #SBATCH -o logs/batch_LM_score/slurm.%A-%a.out # STDOUT
 
 source activate-conda
@@ -13,4 +12,4 @@ conda activate composlang
 
 set -x
 cd ~/code/composlang/notebooks
-python score_csv.py -i ./pairs_with_ctxt.csv -m gpt2 -bsz 64 --part "$SLURM_ARRAY_TASK_ID" --total_parts 500
+python score_csv.py -i ./pairs_with_ctxt.csv -m bert-base-uncased -bsz 32 --part "$SLURM_ARRAY_TASK_ID" --total_parts 500
